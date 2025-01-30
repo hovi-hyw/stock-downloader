444cae5deaf36000c0f7288f4ba690462cf83fe5
hovi-hyw <hovi_hyw@163.com>
2025-01-30 13:40:30 +0800

    Delete all code in devWithAI branch

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-File: models/__init__.py
-"""
-from .base import Base
-from .index_model import IndexDaily
-from .stock_model import StockDaily
-
-__all__ = ['Base', IndexDaily, StockDaily]
--- models/base.py
+++
@@ -1,6 +0,0 @@
-"""
-File: models/base.py
-"""
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
